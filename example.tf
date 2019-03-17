4cc9210cdcc87be0f69d4aa1bd61fd4e563d2de5
oshida <five52113@gmail.com>
2019-03-18 01:16:26 +0900

    edit: VPC情報、security情報追加

--- example.tf
+++ example.tf
@@ -6,10 +6,16 @@ provider "aws" {
 
 # Amazon Linux AMI 2018.03.0
 resource "aws_instance" "example" {
-  ami           = "ami-00a5245b4816c38e6"
+  ami = "ami-00a5245b4816c38e6"
   instance_type = "t2.nano"
+  key_name = "oshida"
+  vpc_security_group_ids = ["sg-05072210c3795d431"]
+  subnet_id = "subnet-069db2d3770201ad0"
+  associate_public_ip_address = true
+  ebs_optimized = false
 
   tags {
     Name = "${format("tf-%02d", count.index + 1)}"
   }
+
 }
