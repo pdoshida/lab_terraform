7ea98ec8d957ca10676b18d673123e56ccf17b0b
oshida <five52113@gmail.com>
2019-03-18 00:33:40 +0900

    new

---
+++ example.tf
@@ -0,0 +1,11 @@
+# profile lab
+provider "aws" {
+  profile = "lab"
+  region = "ap-northeast-1"
+}
+
+# Amazon Linux AMI 2018.03.0
+resource "aws_instance" "example" {
+  ami           = "ami-00a5245b4816c38e6"
+  instance_type = "t2.nano"
+}
